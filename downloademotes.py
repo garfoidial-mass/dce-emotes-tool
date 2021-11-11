2e2e02d4aa240c164cefdd7052d5abc98f30691e
Guildmaster-Garf <62485702+Guildmaster-Garf@users.noreply.github.com>
2021-11-10 19:43:41 -0500

    added the actual script

---
+++ downloademotes.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+import shutil
+import sys
+import os
+
+with open(sys.argv[1]) as fp:
+    soup = BeautifulSoup(fp,"html.parser")
+
+emojilinks = []
+emotes = soup.find_all("img",class_="emoji")
+
+for x in range(0,len(emotes)):
+    if emotes[x]["src"] not in emojilinks:
+        emojilinks.append(emotes[x]["src"])
+
+def findfileformat(link):
+    link = link.replace("https://cdn.discordapp.com/emojis/","",1)
+    newlink = ""
+    for x in range(0,len(link)):
+        if link[x] == ".":
+            link = link[slice(x, len(link),1)]
+            return link
+
+if not os.path.isdir('emotes'):
+    os.mkdir('emotes')
+
+for x in range(0,len(emojilinks)):
+    print(f">\emotes\{emotes[x]['title']}{findfileformat(emojilinks[x])}")
+    r = requests.get(emojilinks[x], stream=True)
+    if r.status_code == 200:                     #200 status code = OK
+        with open(f"{os.getcwd()}\emotes\{emotes[x]['title']}{findfileformat(emojilinks[x])}", 'wb') as f: 
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+for i in range(0,len(emotes)):
+    emotes[i]["src"] = f"{os.getcwd()}\emotes\{emotes[i]['title']}{findfileformat(emojilinks[i])}".strip()
+
+with open(f"{sys.argv[2]}", "w") as f_output:
+    f_output.write(str(soup))  
+
+
+#print(emojilinks)
